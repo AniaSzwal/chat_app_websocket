9e8786c64a0962fc59166d3591c858b1b328b554
Anna Szwal <aniaszwal@gmail.com>
2021-09-03 10:33:30 +0200

    Step 1: add references to HTML

---
+++ app.js
@@ -0,0 +1,7 @@
+const loginForm = document.querySelector('#welcome-form');
+const messagesSection =document.querySelector('#messages-section');
+const messagesList = messagesSection.querySelector('#messages-list');
+const addMessageForm = messagesSection.querySelector('#add-messages-form');
+const userNameInput = loginForm.querySelector('#username');
+const messageContentInput = addMessageForm.querySelector('#message-content');
+
